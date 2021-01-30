505e8c8db45ab3c9f06e41695582c09879ed2770
karanawate <karanawate8@gmail.com>
2021-01-30 06:00:08 +0530

    Design : Grid system in one row in umlimited column

--- resources/views/design/blog.blade.php
+++ resources/views/design/blog.blade.php
@@ -49,33 +49,31 @@
             </div>
          </div>
          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-         <div class="single-blog">
-               <p class="blog-meta">By admin <span>September 24 2018</span></p>
-               <img class ="image-responsive
-               width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
-               <h2 href="">Learn how to learn</h2>
-               <p>
-                Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
-               </p>
-               <p><a href="" class="read-more-btn">Read more</a></p>
-               <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
-            </div>
+            <div class="single-blog">
+                  <p class="blog-meta">By admin <span>September 24 2018</span></p>
+                  <img class ="image-responsive
+                  width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
+                  <h2 href="">Learn how to learn</h2>
+                  <p>
+                  Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
+                  </p>
+                  <p><a href="" class="read-more-btn">Read more</a></p>
+                  <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
+               </div>
          </div>
          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-         <div class="single-blog">
-               <p class="blog-meta">By admin <span>September 24 2018</span></p>
-               <img class ="image-responsive
-               width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
-               <h2 href="">Learn how to learn</h2>
-               <p>
-                Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
-               </p>
-               <p><a href="" class="read-more-btn">Read more</a></p>
-               <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
-            </div>
+            <div class="single-blog">
+                  <p class="blog-meta">By admin <span>September 24 2018</span></p>
+                  <img class ="image-responsive
+                  width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
+                  <h2 href="">Learn how to learn</h2>
+                  <p>
+                  Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
+                  </p>
+                  <p><a href="" class="read-more-btn">Read more</a></p>
+                  <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
+               </div>
          </div>
-      </div>
-      <div class="row">
          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
             <div class="single-blog">
                <p class="blog-meta">By admin <span>September 24 2018</span></p>
@@ -90,30 +88,30 @@
             </div>
          </div>
          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-         <div class="single-blog">
-               <p class="blog-meta">By admin <span>September 24 2018</span></p>
-               <img class ="image-responsive
-               width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
-               <h2 href="">Learn how to learn</h2>
-               <p>
-                Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
-               </p>
-               <p><a href="" class="read-more-btn">Read more</a></p>
-               <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
-            </div>
+            <div class="single-blog">
+                  <p class="blog-meta">By admin <span>September 24 2018</span></p>
+                  <img class ="image-responsive
+                  width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
+                  <h2 href="">Learn how to learn</h2>
+                  <p>
+                  Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
+                  </p>
+                  <p><a href="" class="read-more-btn">Read more</a></p>
+                  <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
+               </div>
          </div>
          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-         <div class="single-blog">
-               <p class="blog-meta">By admin <span>September 24 2018</span></p>
-               <img class ="image-responsive
-               width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
-               <h2 href="">Learn how to learn</h2>
-               <p>
-                Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
-               </p>
-               <p><a href="" class="read-more-btn">Read more</a></p>
-               <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
-            </div>
+            <div class="single-blog">
+                  <p class="blog-meta">By admin <span>September 24 2018</span></p>
+                  <img class ="image-responsive
+                  width:350px;" src="https://i.pinimg.com/originals/7a/1e/56/7a1e56fc58b13e8fc330b1ae97b40018.jpg" class="css-class" alt="alt text">
+                  <h2 href="">Learn how to learn</h2>
+                  <p>
+                  Put life in perspective with some short yet sage pieces of advice. These wise and beautiful words from your favorite positive
+                  </p>
+                  <p><a href="" class="read-more-btn">Read more</a></p>
+                  <span><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>7 people like, <i class="fa fa-comment-o">3..</i></span>
+               </div>
          </div>
       </div>
    </div>
